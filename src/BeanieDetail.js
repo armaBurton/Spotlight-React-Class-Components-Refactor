6133b60732caccb789164aab36235a2c3d48a2f0
armaBurton <armanarma@armaburton.com>
2022-02-10 21:06:30 -0800

    removed console.logs

--- src/BeanieDetail.js
+++ src/BeanieDetail.js
@@ -1,50 +1,7 @@
-import { useEffect, useState } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { getSingleBeanie } from './services/fetch-utils';
 import './App.css';
 import { Component } from 'react';
-
-
-// export default function BeanieDetail() {
-
-//   const [beanieBaby, setBeanieBaby] = useState({});
-//   const params = useParams();
-//   useEffect(() => {
-   
-//     async function onLoad() {
-//       const data = await getSingleBeanie(params.id);
-//       setBeanieBaby(data);
-//     }
-
-//     onLoad();
-//   }, [params.id]); 
-
-//   return (
-//     <Link to={`/`}>
-//       <div className='bean-holder'>
-//         <img className='detail-image' src={beanieBaby.image} alt={beanieBaby.title} />
-//         <div className='bean-card'>
-//           <h2 className='title'>{beanieBaby.title}</h2>
-//           <p className='zodiac'>Zodiac: {beanieBaby.astroSign}</p>
-//           <p className="born">Born: {beanieBaby.birthday}</p>
-//           <p className="release-date">Release Date: {beanieBaby.releaseDate}</p>
-//           <p className="style-number">Styling Number: {beanieBaby.styleNumber}</p>
-//         </div>
-//       </div>
-//     </Link>
-//   );
-// }
-// const params = new URLSearchParams(window.location.search);
-// const id = params.get('id');
-
-// let windowData = window.location;
-// console.log(windowData);
-// let split = windowData.href.split('/');
-// let count = split.length - 1;
-// console.log(split[count]);
-
-
-
 
 export default class BeanieDetail extends Component {
   constructor() {
@@ -70,9 +27,8 @@ export default class BeanieDetail extends Component {
     
     const { beanieBaby } = this.state;
     if (beanieBaby.beanieBaby === undefined){
-      console.log(`wait`);
+      <></>;
     } else {
-      console.log(beanieBaby);
       return <Link to={`/`}>
         <div className='bean-holder'>
           <img className='detail-image' src={beanieBaby.beanieBaby.image} alt={beanieBaby.beanieBaby.title} />
